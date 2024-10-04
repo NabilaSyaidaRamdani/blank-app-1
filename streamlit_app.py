dadda9234d9dc727b1a03aeff984cb8b88bc7458
NabilaSyaidaRamdani <129932731+NabilaSyaidaRamdani@users.noreply.github.com>
2024-10-04 08:32:06 +0700

    Update streamlit_app.py
    change the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,47 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Welcome to My Streamlit App!")
+
+st.header('HI! Our Dear Customer')
+
+st.subheader('Please fill Your Name')
+
+Name = st.text_input(label='Full Name', value='')
+
+City = st.text_input(label='City', value='')
+
+State = st.text_input(label='State', value='')
+ 
+with st.sidebar:
+    
+    st.text('Ini merupakan sidebar')
+    
+    values = st.slider(
+        label='Select a range of values',
+        min_value=0, max_value=100, value=(0, 100)
+    )
+    st.write('Values:', values)
+    
+import streamlit as st
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url('https://i.pinimg.com/564x/ed/6b/be/ed6bbe5838979e5bca84c998cd2a87e6.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
 )
+
+if st.button("Sign In"):
+    st.write("You clicked on Sign In!")
+    # Tambahkan logika untuk proses Sign In di sini
+
+# Tombol Sign Up
+if st.button("Sign Up"):
+    st.write("You clicked on Sign Up!")
+    # Tambahkan logika untuk proses Sign Up di sini
